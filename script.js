52f387fc93cf02f52afd2d07396a99ae51180e13
Masum Billah Tarek <mdmasumbillah245524@gmail.com>
2025-05-13 09:30:59 +0600

    1st

---
+++ script.js
@@ -0,0 +1,11 @@
+var shoppingCard={
+    books:3,
+    sunglass:1,
+    keyboard:5,
+    mouse:1,
+    pen:25
+}
+var penCount=shoppingCard.pen;
+console.log(penCount);
+var penCount2=shoppingCard['pen'];
+console.log(penCount2);
